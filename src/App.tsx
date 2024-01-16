143f971f00fea41fc2ca818787d1b25b2805fdad
KTG <xorb269@naver.com>
2024-01-17 08:26:14 +0900

    init: v2 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -6,75 +6,119 @@ interface body {
   msg: string;
 }
 
-function App() {
-  const [server, setServer] = useState("70.12.114.70:8080/websocketserver");
-  const [subTo, setSubTo] = useState("/sub/chat/enter");
-  const [pubTo, setPubTo] = useState("/sub/chat/enter");
+interface chatRoom {
+  name: string;
+  url: string;
+}
 
-  const [msg, setMsg] = useState("Hi");
+function App() {
+  const [id, setId] = useState("ktg");
+  const [pw, setPw] = useState("1234");
   const [nickname, setNickname] = useState("ktg");
 
+  const [chatRooms, setChatRooms] = useState<chatRoom[]>([
+    { name: "기본 채팅방", url: "/sub/chat/enter" },
+  ]);
+
+  const [sub, setSub] = useState("");
+  const [msg, setMsg] = useState("Hi");
   const [logs, setLogs] = useState<body[]>([{ nickname: "ktg", msg: "hi" }]);
 
   const client = new Client({
+    brokerURL: "ws://70.12.114.70:8080/websocketserver",
     reconnectDelay: 5000,
     heartbeatIncoming: 4000,
     heartbeatOutgoing: 4000,
   });
 
   // 이벤트 핸들러
-  const handleServer = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleId = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setServer(value);
+    setId(value);
   };
+  const handlePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setPw(value);
+  };
+  const handleLogin = () => {
+    setNickname(id);
+    client.activate();
+  };
+
   const handleMsg = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setMsg(value);
   };
-  const handleSubTo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setSubTo(value);
-  };
-  const handlePubTo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setPubTo(value);
-  };
-  const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setNickname(value);
-  };
+
   const handlePublish = () => {
     if (client.connected) {
       const body = { nickname: nickname, msg: msg };
       const strBody = JSON.stringify(body);
-      client.publish({ destination: pubTo, body: strBody });
+      client.publish({ destination: "/sub/chat/enter", body: strBody });
     } else {
       console.error("STOMP connection is not active. Cannot publish.");
     }
   };
-  const handleSubscribe = () => {
+  const handleSubscribe = (e) => {
+    const url = e.target.dataset.url;
+    if (sub !== "") {
+      alert("채널을 먼저 나가주세요.");
+      return;
+    }
     if (client.connected) {
-      client.subscribe(subTo, (e) => {
+      client.subscribe(url, (e) => {
         const parseBody: body = JSON.parse(e.body);
         setLogs([...logs, parseBody]);
+        setSub(url);
       });
     } else {
-      console.error("STOMP connection is not active. Cannot subscribe.");
+      console.error("연결된 서버가 없습니다.");
+    }
+  };
+  const handleUnsubscribe = (e) => {
+    const url = e.target.dataset.url;
+    if (sub === "") {
+      alert("구독한 채널이 없습니다.");
+      return;
+    }
+    if (client.connected) {
+      client.unsubscribe(url);
+      setLogs([]);
+      setSub("");
+    } else {
+      console.error("연결된 서버가 없습니다.");
     }
   };
 
   // styles
-  const wrapper = {
+  const wrapper = { display: "flex" };
+  const login = {
+    flex: 1,
     display: "flex",
+    flexDirection: "column",
+    alignItems: "end",
+    margin: "0 20px",
   };
-  const divStyle = {
+
+  const chatUl = {
+    flex: 1,
+    width: "300px",
+    height: "600px",
+    padding: "20px",
+    margin: "0 20px",
+    border: "1px solid black",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "start",
-    gap: "30px",
+    flexDirection: "column",
   };
-  const setting = {};
+  const chatLi = {
+    borderTop: "1px solid black",
+    padding: "10px 0",
+    display: "flex",
+    justifyContent: "space-between",
+  };
+
   const chatroom = {
+    flex: 1,
     width: "300px",
     height: "600px",
     padding: "20px",
@@ -87,58 +131,38 @@ function App() {
   const chatbody = { flex: 1, display: "flex", flexDirection: "column" };
   const chat = { display: "flex" };
   const mychat = { display: "flex", flexDirection: "row-reverse" };
-  const chatinput = {
-    borderTop: "1px solid black",
-    display: "flex",
-  };
+  const chatinput = { borderTop: "1px solid black", display: "flex" };
 
   return (
     <div style={wrapper}>
-      <div style={setting}>
-        <div style={divStyle}>
-          <h3>Set Server</h3>
-          <input
-            onChange={handleServer}
-            value={server}
-            style={{ width: "250px" }}
-          />
-          <button onClick={() => (client.brokerURL = `ws://${server}`)}>
-            Set
-          </button>
-        </div>
-        <hr />
-        <div style={divStyle}>
-          <h3>Active/Deactive Button</h3>
-          <button onClick={() => client.activate()}>activate</button>
-          <button onClick={() => client.deactivate()}>deactivate</button>
-        </div>
-        <hr />
-        <div style={divStyle}>
-          <h3>Subscribe</h3>
-          <p>
-            To: <input onChange={handleSubTo} value={subTo} />
-          </p>
-          <button onClick={handleSubscribe}>subscribe</button>
-        </div>
-        <hr />
-        <div style={divStyle}>
-          <h3>Publish</h3>
-          <div>
-            <p>
-              To: <input onChange={handlePubTo} value={pubTo} />
-            </p>
-          </div>
-        </div>
-        <hr />
+      <div style={login}>
+        <p>
+          ID: <input value={id} onChange={handleId} />
+        </p>
+        <p>
+          PW: <input value={pw} onChange={handlePw} />
+        </p>
+        <button onClick={handleLogin}>login</button>
+      </div>
+      <div style={chatUl}>
+        <h2>Chat room list</h2>
+        {chatRooms.map((chatRoom, idx) => {
+          return (
+            <div
+              key={idx}
+              style={chatLi}
+              data-url={chatRoom.url}
+              onClick={handleSubscribe}
+            >
+              {chatRoom.name}
+              <button onClick={handleUnsubscribe}>x</button>
+            </div>
+          );
+        })}
       </div>
       <div style={chatroom}>
         <div style={chatheader}>
-          <span>Nickname:</span>
-          <input
-            style={{ flex: 1 }}
-            onChange={handleNickname}
-            value={nickname}
-          />
+          <span>Nickname: {nickname}</span>
         </div>
         <div style={chatbody}>
           {logs.map((chatlog, idx) => {
