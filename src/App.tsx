5cdc3954bbf9b74ef0be68722bf14d1ff677f5df
KTG <xorb269@naver.com>
2024-01-17 07:10:40 +0900

    feat: 채팅방 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { Client } from "@stomp/stompjs";
 import React, { useState } from "react";
 
+interface body {
+  nickname: string;
+  msg: string;
+}
+
 function App() {
   const [server, setServer] = useState("70.12.114.70:8080/websocketserver");
+  const [subTo, setSubTo] = useState("/sub/chat/enter");
+  const [pubTo, setPubTo] = useState("/sub/chat/enter");
+
   const [msg, setMsg] = useState("Hi");
-  const [to, setTo] = useState("/sub/chat/enter");
+  const [nickname, setNickname] = useState("ktg");
+
+  const [logs, setLogs] = useState<body[]>([{ nickname: "ktg", msg: "hi" }]);
 
   const client = new Client({
-    debug: function (str) {
-      console.log(str);
-    },
     reconnectDelay: 5000,
     heartbeatIncoming: 4000,
     heartbeatOutgoing: 4000,
@@ -24,71 +31,141 @@ function App() {
     const { value } = e.target;
     setMsg(value);
   };
-  const handleTo = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubTo = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setTo(value);
+    setSubTo(value);
+  };
+  const handlePubTo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setPubTo(value);
+  };
+  const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNickname(value);
   };
   const handlePublish = () => {
     if (client.connected) {
-      client.publish({ destination: to, body: msg });
+      const body = { nickname: nickname, msg: msg };
+      const strBody = JSON.stringify(body);
+      client.publish({ destination: pubTo, body: strBody });
     } else {
       console.error("STOMP connection is not active. Cannot publish.");
     }
   };
   const handleSubscribe = () => {
     if (client.connected) {
-      client.subscribe(to, () => {});
+      client.subscribe(subTo, (e) => {
+        const parseBody: body = JSON.parse(e.body);
+        setLogs([...logs, parseBody]);
+      });
     } else {
       console.error("STOMP connection is not active. Cannot subscribe.");
     }
   };
 
+  // styles
+  const wrapper = {
+    display: "flex",
+  };
   const divStyle = {
-    display: "grid",
+    display: "flex",
     alignItems: "center",
-    justifyContent: "center",
-    gridTemplateColumns: "1fr 2fr 2fr",
-    margin: "50px",
+    justifyContent: "start",
     gap: "30px",
+  };
+  const setting = {};
+  const chatroom = {
+    width: "300px",
+    height: "600px",
+    padding: "20px",
+    margin: "0 20px",
+    border: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+  };
+  const chatheader = { borderBottom: "1px solid black", display: "flex" };
+  const chatbody = { flex: 1, display: "flex", flexDirection: "column" };
+  const chat = { display: "flex" };
+  const mychat = { display: "flex", flexDirection: "row-reverse" };
+  const chatinput = {
+    borderTop: "1px solid black",
+    display: "flex",
   };
 
   return (
-    <div>
-      <div style={divStyle}>
-        <h3>Set Server</h3>
-        <input
-          onChange={handleServer}
-          value={server}
-          style={{ width: "100%" }}
-        />
-        <button onClick={() => (client.brokerURL = `ws://${server}`)}>
-          Set
-        </button>
+    <div style={wrapper}>
+      <div style={setting}>
+        <div style={divStyle}>
+          <h3>Set Server</h3>
+          <input
+            onChange={handleServer}
+            value={server}
+            style={{ width: "250px" }}
+          />
+          <button onClick={() => (client.brokerURL = `ws://${server}`)}>
+            Set
+          </button>
+        </div>
+        <hr />
+        <div style={divStyle}>
+          <h3>Active/Deactive Button</h3>
+          <button onClick={() => client.activate()}>activate</button>
+          <button onClick={() => client.deactivate()}>deactivate</button>
+        </div>
+        <hr />
+        <div style={divStyle}>
+          <h3>Subscribe</h3>
+          <p>
+            To: <input onChange={handleSubTo} value={subTo} />
+          </p>
+          <button onClick={handleSubscribe}>subscribe</button>
+        </div>
+        <hr />
+        <div style={divStyle}>
+          <h3>Publish</h3>
+          <div>
+            <p>
+              To: <input onChange={handlePubTo} value={pubTo} />
+            </p>
+          </div>
+        </div>
+        <hr />
       </div>
-      <hr />
-      <div style={divStyle}>
-        <h3>Active/Deactive Button</h3>
-        <button onClick={() => client.activate()}>activate</button>
-        <button onClick={() => client.deactivate()}>deactivate</button>
+      <div style={chatroom}>
+        <div style={chatheader}>
+          <span>Nickname:</span>
+          <input
+            style={{ flex: 1 }}
+            onChange={handleNickname}
+            value={nickname}
+          />
+        </div>
+        <div style={chatbody}>
+          {logs.map((chatlog, idx) => {
+            if (chatlog.nickname === nickname) {
+              return (
+                <p key={idx} style={mychat}>
+                  <span>{chatlog.nickname}</span>
+                  <span>&nbsp;-&nbsp;</span>
+                  <span>{chatlog.msg}</span>
+                </p>
+              );
+            } else {
+              return (
+                <p key={idx} style={chat}>
+                  <span>{chatlog.nickname}</span>
+                  <span> : </span>
+                  <span>{chatlog.msg}</span>
+                </p>
+              );
+            }
+          })}
+        </div>
+        <div style={chatinput}>
+          <input style={{ flex: 1 }} onChange={handleMsg} value={msg} />
+          <button onClick={handlePublish}>send</button>
+        </div>
       </div>
-      <hr />
-      <div style={divStyle}>
-        <h3>Subscribe</h3>
-        <p>
-          To: <input onChange={handleTo} value={to} style={{ width: "90%" }} />
-        </p>
-        <button onClick={handleSubscribe}>subscribe</button>
-      </div>
-      <hr />
-      <div style={divStyle}>
-        <h3>Publish</h3>
-        <p>
-          Msg:{" "}
-          <input onChange={handleMsg} value={msg} style={{ width: "85%" }} />
-        </p>
-        <button onClick={handlePublish}>publish</button>
-      </div>
-      <hr />
     </div>
   );
 }
