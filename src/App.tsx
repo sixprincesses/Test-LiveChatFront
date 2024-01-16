a98969d8579321c96c34edd50990543f7a55302b
KTG <xorb269@naver.com>
2024-01-16 16:45:34 +0900

    Feat: 서버설정, 연결, 구독, 전송 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { Client } from "@stomp/stompjs";
 import React, { useState } from "react";
 
-const client = new Client({
-  brokerURL: "ws://70.12.114.70:8080/websocketserver",
-  debug: function (str) {
-    console.log(str);
-  },
-  reconnectDelay: 5000,
-  heartbeatIncoming: 4000,
-  heartbeatOutgoing: 4000,
-});
-
 function App() {
+  const [server, setServer] = useState("");
   const [msg, setMsg] = useState("");
   const [to, setTo] = useState("");
 
+  const client = new Client({
+    debug: function (str) {
+      console.log(str);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+  });
+
+  // 이벤트 핸들러
+  const handleServer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setServer(value);
+  };
   const handleMsg = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setMsg(value);
@@ -31,22 +36,60 @@ function App() {
     }
   };
   const handleSubscribe = () => {
-    client.subscribe("/sub/chat/enter", () => {});
+    if (client.connected) {
+      client.subscribe(to, () => {});
+    } else {
+      console.error("STOMP connection is not active. Cannot subscribe.");
+    }
+  };
+
+  const divStyle = {
+    display: "grid",
+    alignItems: "center",
+    justifyContent: "center",
+    gridTemplateColumns: "1fr 2fr 2fr",
+    margin: "50px",
+    gap: "30px",
   };
 
   return (
-    <>
-      <button onClick={() => client.activate()}>activate</button>
-      <button onClick={() => client.deactivate()}>deactivate</button>
-      <button onClick={handleSubscribe}>subscribe</button>
-      <button onClick={handlePublish}>publish</button>
-      <p>
-        To: <input onChange={handleTo} value={to} />
-      </p>
-      <p>
-        Msg: <input onChange={handleMsg} value={msg} />
-      </p>
-    </>
+    <div>
+      <div style={divStyle}>
+        <h3>Set Server</h3>
+        <input
+          onChange={handleServer}
+          value={server}
+          style={{ width: "100%" }}
+        />
+        <button onClick={() => (client.brokerURL = `ws://${server}`)}>
+          Set
+        </button>
+      </div>
+      <hr />
+      <div style={divStyle}>
+        <h3>Active/Deactive Button</h3>
+        <button onClick={() => client.activate()}>activate</button>
+        <button onClick={() => client.deactivate()}>deactivate</button>
+      </div>
+      <hr />
+      <div style={divStyle}>
+        <h3>Subscribe</h3>
+        <p>
+          To: <input onChange={handleTo} value={to} style={{ width: "90%" }} />
+        </p>
+        <button onClick={handleSubscribe}>subscribe</button>
+      </div>
+      <hr />
+      <div style={divStyle}>
+        <h3>Publish</h3>
+        <p>
+          Msg:{" "}
+          <input onChange={handleMsg} value={msg} style={{ width: "85%" }} />
+        </p>
+        <button onClick={handlePublish}>publish</button>
+      </div>
+      <hr />
+    </div>
   );
 }
 
